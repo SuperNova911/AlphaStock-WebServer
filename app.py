644da0e296d9b3cbde7ff1cb626f9fc52685b02e
Suwhan Kim <suwhan77@naver.com>
2020-05-14 01:53:15 +0900

    테스트 코드 수정

--- app.py
+++ app.py
@@ -6,18 +6,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/api/auth/<email>/<token>')
+def auth(email, token):
+    return email + ", " + token
+
+
+@app.route('/api/unsubscribe/<email>/<token>')
+def unsubscribe(email, token):
+    return email + ", " + token
+
+
 @app.route('/elements')
 def elements():
     return render_template('elements.html')
 
+
 @app.route('/stock/<fappa>')
 def stock(fappa):
     return 'Stock: ' + fappa
+
 
 @app.route('/json')
 def json():
     data = { 'name' : 'Kappa', 'Family' : 'Ross' }
     return jsonify(data)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+    
